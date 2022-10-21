ffd14bb2f993e1c633e4eb5a7a6174e73d8ed3c4
beto-rdz <brandon.48beto@gmail.com>
2022-10-21 13:22:12 -0500

    Release 21-10-2022
    Se carga Actividad didáctica correspondiente.

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,14 +10,14 @@
             img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Nombre de la actividad
-        p.mb-4 #[b Objetivo] Cras tempus tortor et nisi tincidunt feugiat. Integer lacus ante, venenatis nec fringilla quis, dapibus ut dui. Fusce imperdiet velit a leo sagittis, id scelerisque massa placerat. Duis at dui sed enim euismod pulvinar quis ac ante. Phasellus condimentum sapien nisi, ac facilisis purus tristique ac. Quisque magna est, luctus vitae elit ut, convallis venenatis justo. Nunc pretium elit a nisi tempus sagittis.
+          h2 Actividad didáctica
+        p.mb-4 #[b Objetivo] Desarrolle la siguiente actividad y pondrá a prueba los conocimientos desarrollados en esta unidad de aprendizaje. Conteste falso o verdadero según corresponda al enunciado.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Tipo de actividad
+              p.fw-bold.mb-0 Verdadero y falso
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/Actividad_didactica_CF_5_623800/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
