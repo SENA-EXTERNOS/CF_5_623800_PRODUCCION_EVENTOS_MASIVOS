997868b28fe1032a55bb8d053b2e1c0384bd83ff
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-07-28 10:34:54 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -244,7 +244,8 @@ export default {
     {
       referencia:
         'Resolución 1409 de 2012 [Ministerio del Trabajo] Por la cual se establece el Reglamento de Seguridad para protección contra caídas en trabajo en alturas. 23 de julio de 2012',
-      link: 'https://www.mintrabajo.gov.co/documents/20147/45107/resolucion_00001409_de_2012.pdf',
+      link: 
+        'https://www.mintrabajo.gov.co/documents/20147/45107/resolucion_00001409_de_2012.pdf',
     },
     {
       referencia: 'Seguridad Global (2021). EPP para Trabajos en Altura.',
